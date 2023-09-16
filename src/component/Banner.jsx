2b234e1153a2e896638f877ae8c2eb50e9e633f6
Sanjeev <sanjeev@convin.ai>
2023-09-17 00:57:40 +0530

    text change

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -7,9 +7,9 @@ const Banner = () => {
       <div className='banner  bg-[#1A1A1A] pb-3 text-white '>
         <div className='background  container mx-auto '>
           <div className='banner-text font-extrabold text-[8.4rem] text-white pt-[9.3rem]   '>
-            <p className=''>FRESH</p>
+            <p className=''>Fresh</p>
             <p className='texttransparent flex justify-center mr-6'>2022</p>
-            <p>LOOKS</p>
+            <p>Looks</p>
           </div>
           <p className='mt-24  border inline-flex border-white p-0.5 '>
             see more <FiArrowUpRight className='mt-1 ml-1' />
